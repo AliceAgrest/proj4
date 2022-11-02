9ee6e61c5334d37308f02830e03f7728838bf6b7
Alice Agrest <114485470+AliceAgrest@users.noreply.github.com>
2022-11-02 12:58:52 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace proj4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int meot, ahadot, asarot,rndnum, memuza = 0, schum = 0;
+            Random rnd = new Random();
+            
+            for (int i = 100; i < 1000; i++)
+            {
+                rndnum = rnd.Next(100, 1000);
+                meot = rndnum / 100;
+                asarot = (rndnum / 10) % 10;
+                ahadot = rndnum % 10;
+
+                if ((ahadot + asarot == meot) && (meot - asarot == ahadot))
+                {
+                    Console.WriteLine($"Your lucky number is {rndnum}");
+                    schum = schum + rndnum;
+                    memuza = memuza + 1;
+                }
+            }
+            Console.WriteLine($"The average of all lucky numbers is {schum / memuza}");
+
+            Console.ReadLine();
         }
     }
 }
